da40e5f1cb3c9c5bd0ca72d239c250691eabcc40
Pasquale <popoliziop@yahoo.com>
2023-11-14 18:13:12 -0500

    Began work on NavMesh portion of VsauceBoss AI.  The boss now patrols when the player is not in range, chases the player when they are in range, and attacks when the playere is really close.  Still a work in progress.

--- Assets/VsauceBossAI.cs
+++ Assets/VsauceBossAI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.AI.Navigation;
 
 public class VsauceBossAI : MonoBehaviour
 {
@@ -15,8 +16,29 @@ public class VsauceBossAI : MonoBehaviour
     [SerializeField]private GameObject explosionEffect;
     [SerializeField]private GameObject rightHand;
     private AudioSource aSource;
-    private GameObject player;
+    
     private Animator animator;
+
+    //Navmesh variables
+    public UnityEngine.AI.NavMeshAgent agent;
+
+    private GameObject player;
+
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    //moving
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    //States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
     // Start is called before the first frame update
 
     void Start()
@@ -24,6 +46,7 @@ public class VsauceBossAI : MonoBehaviour
         player = GameObject.FindGameObjectWithTag("Player");
         animator = gameObject.GetComponentInChildren<Animator>();
         aSource = gameObject.GetComponent<AudioSource>();
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
     }
 
     // Update is called once per frame
@@ -54,6 +77,57 @@ public class VsauceBossAI : MonoBehaviour
             slag();
         }
         action = 0;
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!playerInSightRange && !playerInAttackRange) patrolling();
+        if (playerInSightRange && !playerInAttackRange) chasePlayer();
+        if (playerInSightRange && playerInAttackRange) attackPlayer();
+    }
+
+    private void patrolling()
+    {
+        if(!walkPointSet) 
+            searchWalkPoint();
+
+        if(walkPointSet)
+            agent.SetDestination(walkPoint);
+            gameObject.transform.LookAt(walkPoint);
+            walk();
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        if(distanceToWalkPoint.magnitude <1f)
+            walkPointSet = false;
+            stopWalking();
+    }
+
+    private void searchWalkPoint()
+    {
+        //calculate random point in range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 5f, whatIsGround))
+        {
+            playVoiceLine();
+            walkPointSet = true;
+        }
+    }
+
+    private void chasePlayer()
+    {
+        agent.SetDestination(player.transform.position);
+        Vector3 temp = new Vector3(player.transform.position.x, gameObject.transform.position.y, player.transform.position.z);
+        gameObject.transform.LookAt(temp);
+        walk();
+    }
+
+    private void attackPlayer()
+    {
+        punchR();
     }
 
     private void idle()
@@ -63,22 +137,22 @@ public class VsauceBossAI : MonoBehaviour
 
     private void walk()
     {
-        Vector3 temp = new Vector3(player.transform.position.x, gameObject.transform.position.y, player.transform.position.z);
-        gameObject.transform.LookAt(temp);
         animator.Play("Walk");
-        playVoiceLine();
+        //playVoiceLine();
     }
 
     private void punchL()
     {
         animator.Play("Base.PunchL");
-        aSource.PlayOneShot(punchAC);
+        aSource.clip = punchAC;
+        aSource.Play();
     }
 
     private void punchR()
     {
         animator.Play("Base.PunchR");
-        aSource.PlayOneShot(punchAC);
+        aSource.clip = punchAC;
+        aSource.Play();
     }
 
     private void thermiteBallsShort()
@@ -106,6 +180,18 @@ public class VsauceBossAI : MonoBehaviour
             }
         GameObject temp = Instantiate(explosionEffect, rightHand.transform);
         Destroy(temp, 1.0f);
+    }
+
+    IEnumerator stopWalking()
+    {
+        float delayInMs = 2000f;
+        float ms = Time.deltaTime;
+        idle();
+        while(ms <= delayInMs )
+            {
+                ms += Time.deltaTime;
+                yield return null;
+            }
     }
 
     private void playVoiceLine()
