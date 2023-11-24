e6e9a282c1f634a5b332dfaeaa83d79e47629997
420noscope-exe <popoliziop@yahoo.com>
2023-11-24 12:58:31 -0500

    Started work on the AI for a new enemy named Android.

---
+++ Assets/Scripts/AndroidAi.cs
@@ -0,0 +1,191 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AndroidAi : MonoBehaviour
+{
+    [SerializeField]private List<AudioClip> clips = new List<AudioClip>();
+    [SerializeField]private List<AudioClip> hit = new List<AudioClip>();
+
+    private AudioSource aSource;
+    
+    private Animator animator;
+
+    //Navmesh variables
+    public UnityEngine.AI.NavMeshAgent agent;
+
+    private GameObject player;
+
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    //moving
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //Attacking
+    public float timeBetweenAttacks, timBetweenVoiceLines, timeBetweenPatrol;
+    bool alreadyAttacked, alreadyVoiceLined, alreadyPatrolled, alreadyHitPlayer;
+    [SerializeField]private GameObject projectile, muzzle;
+
+    //States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        animator = gameObject.GetComponentInChildren<Animator>();
+        aSource = gameObject.GetComponent<AudioSource>();
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!playerInSightRange && !playerInAttackRange && !alreadyPatrolled) patrolling();
+        if (playerInSightRange && !playerInAttackRange && !alreadyAttacked) chasePlayer();
+        if (playerInSightRange && playerInAttackRange) attackPlayer();
+    }
+
+    //The three states for the state machine
+    private void patrolling()
+    {
+        if(!walkPointSet) 
+            searchWalkPoint();
+
+        if(walkPointSet)
+            agent.SetDestination(walkPoint);
+            //gameObject.transform.LookAt(walkPoint);
+            walk();
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        if(distanceToWalkPoint.magnitude <1f)
+        {
+            walkPointSet = false;
+            if(!alreadyPatrolled)
+            {
+                idle();
+                alreadyPatrolled = true;
+                Invoke(nameof(resetPatrol), timeBetweenPatrol);
+            }
+        }    
+
+        if(!alreadyVoiceLined)
+            {
+                //playVoiceLine();
+                alreadyVoiceLined = true;
+                Invoke(nameof(resetVoiceline), timBetweenVoiceLines);
+            }
+
+    }
+
+    private void chasePlayer()
+    {
+        agent.SetDestination(player.transform.position);
+        Vector3 temp = new Vector3(player.transform.position.x, gameObject.transform.position.y, player.transform.position.z);
+        gameObject.transform.LookAt(temp);
+        walk();
+        if(!alreadyVoiceLined)
+            {
+                //playVoiceLine();
+                alreadyVoiceLined = true;
+                Invoke(nameof(resetVoiceline), timBetweenVoiceLines);
+            }
+    }
+
+    private void attackPlayer()
+    {
+        agent.SetDestination(transform.position);
+
+        if(!alreadyAttacked)
+        {
+            resetHitPlayer();
+            agent.Stop();
+            attack();
+            alreadyAttacked = true;
+            Invoke(nameof(resetAttack), timeBetweenAttacks);
+            Invoke(nameof(resumeAgent), timeBetweenAttacks);
+        }    
+    }
+
+    //Search walkpoint for navmesh, helper function for patrolling
+    private void searchWalkPoint()
+    {
+        //calculate random point in range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 5f, whatIsGround))
+        {
+            walkPointSet = true;
+        }
+    }
+
+    //Damages any HealthController that collides with thing GameObject while it is attacking.
+    public void OnCollisionEnter(Collision col)
+    {
+        if(alreadyAttacked && !alreadyHitPlayer && col.gameObject.GetComponent<HealthController>() != null)
+        {
+            col.gameObject.GetComponent<HealthController>().takeDamage(20);
+            alreadyHitPlayer = true;
+        }
+    }
+
+    //reset functions for State Machine
+    private void resetAttack()
+    {
+        alreadyAttacked = false;
+    }
+
+    private void resetVoiceline()
+    {
+        alreadyVoiceLined = false;
+    }
+
+    private void resetPatrol()
+    {
+        alreadyPatrolled = false;
+    }
+
+    private void resetHitPlayer()
+    {
+        alreadyHitPlayer = false;
+    }
+
+    public void resumeAgent()
+    {
+        agent.Resume();
+    }
+    //Animations
+    private void attack()
+    {
+        animator.Play("Attack");
+    }
+
+    private void idle()
+    {
+        animator.Play("Base.Idle");
+    }
+
+    private void walk()
+    {
+        animator.Play("Walk");
+        //playVoiceLine();
+    }
+
+    //Idle and walk voicelines
+    private void playVoiceLine()
+    {
+        aSource.clip = clips[Random.Range(0,clips.Count)];
+        aSource.Play();
+    }
+}
